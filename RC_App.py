5ad351730db27506d933a02734ae7e919cefd3b0
NAJMI <mnajmi2912@gmail.com>
2024-04-26 23:30:34 +0800

    add pict

--- RC_App.py
+++ RC_App.py
@@ -178,7 +178,9 @@ def instruction_page():
     col1, col2, col3 = st.columns(3)
 
     with col2:
+        st.image(r"logo.jpg")
         st.title("About us")
+    
 
     text = """"Our Mission:  At Reader Compass, we're passionate about igniting a love of reading and empowering every reader to find their perfect match.
 
